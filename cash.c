75675603c8fff5899705088d25ea943321ea57c3
AKCcoding <alraeatwork@gmail.com>
2023-06-02 00:08:25 +0800

    added cash.c practice file

---
+++ cash.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "cs50.h"
+#include "cs50.c"
+#include <math.h>
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+int main(void)
+{
+    int cents = get_cents();
+
+    int quarters = calculate_quarters(cents);
+    cents -= quarters * 25;
+
+    int dimes = calculate_dimes(cents);
+    cents -= dimes * 10;
+
+    int nickels = calculate_nickels(cents);
+    cents -= nickels * 5;
+
+    int pennies = calculate_pennies(cents);
+    cents -= pennies;
+
+    int total_coins = quarters + dimes + nickels + pennies;
+    printf("%d\n", total_coins);
+}
+
+int get_cents(void)
+{
+    int cents;
+    do
+    {
+        cents = get_int("Change owed: ");
+    }
+    while (cents < 0);
+
+    return cents;
+}
+
+int calculate_quarters(int cents)
+{
+    return cents / 25;
+}
+
+int calculate_dimes(int cents)
+{
+    return cents / 10;
+}
+
+int calculate_nickels(int cents)
+{
+    return cents / 5;
+}
+
+int calculate_pennies(int cents)
+{
+    return cents;
+}
